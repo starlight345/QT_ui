961d9ce2e81e7ef4daa84804fb271fa6560c739e
SingSeung <124044703+SingSeung@users.noreply.github.com>
2023-02-03 16:53:40 +0900

    Update test3.py

--- test3.py
+++ test3.py
@@ -133,11 +133,15 @@ class Ui_MainWindow(object):
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
 "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">Lower View</p></body></html>"))
+        
+        #Grade : 을 나타내는 창
         self.textBrowser.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'Ubuntu\'; font-size:11pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Grade : </span></p></body></html>"))
+        
+        #grade를 표현함
         self.label_7.setText(_translate("MainWindow", grade))
         self.label_7.raise_()
 
